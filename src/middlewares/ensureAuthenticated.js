acbfbdd85b835448f3fe674e16af6440d89aeeda
lucas <lucas327ks@gmail.com>
2023-07-21 21:34:33 -0300

    fix: adjusted token send format

--- src/middlewares/ensureAuthenticated.js
+++ src/middlewares/ensureAuthenticated.js
@@ -1,29 +1,26 @@
-const {verify} = require("jsonwebtoken")
-const authConfig = require("../configs/auth")
-const AppError = require("../utils/AppError")
+const { verify } = require('jsonwebtoken')
+const authConfig = require('../configs/auth')
+const AppError = require('../utils/AppError')
 
-function ensureAuthenticated(req, res, next){
+function ensureAuthenticated(req, res, next) {
+	const authHeader = req.headers.authorization
 
-  const authHeader = req.headers.authorization
+	if (!authHeader) {
+		throw new AppError('JWT Token inválido!', 401)
+	}
 
-  if(!authHeader){
-    throw new AppError("JWT Token inválido!", 401)
-  }
+	const [, token] = authHeader.split(' ')
 
-  const [token] = authHeader.split(" ")
+	try {
+		const { sub: user_id } = verify(token, authConfig.jwt.secret)
 
-  try{
-    const {sub: user_id} = verify(token, authConfig.jwt.secret)
-
-    req.user = {
-      id: Number(user_id)
-    }
-    return next()
-  }
-  catch{
-    throw new AppError("JWT Token inválido!", 401)
-  }
-
+		req.user = {
+			id: Number(user_id)
+		}
+		return next()
+	} catch {
+		throw new AppError('JWT Token inválido!', 401)
+	}
 }
 
 module.exports = ensureAuthenticated
