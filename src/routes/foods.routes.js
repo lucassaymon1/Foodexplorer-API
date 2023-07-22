a30982d25dbc1299882ba3021562e11e98f004ff
lucas <lucas327ks@gmail.com>
2023-07-21 21:35:33 -0300

    feat: add multer config for images receiving

--- src/routes/foods.routes.js
+++ src/routes/foods.routes.js
@@ -1,20 +1,28 @@
-const {Router} = require("express")
-const FoodsController = require("../controllers/FoodsController")
-const FoodsPictureController = require("../controllers/FoodsPictureController")
-const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
+const { Router } = require('express')
+const FoodsController = require('../controllers/FoodsController')
+const FoodsPictureController = require('../controllers/FoodsPictureController')
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
-const foodsController = new FoodsController
-const foodsPictureController = new FoodsPictureController
+const multer = require('multer')
+const uploadConfig = require('../configs/uploads')
+
+const foodsController = new FoodsController()
+const foodsPictureController = new FoodsPictureController()
 
 const foodsRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 foodsRoutes.use(ensureAuthenticated)
 
-foodsRoutes.get("/:id", foodsController.show)
-foodsRoutes.get("/", foodsController.index)
-foodsRoutes.post("/", foodsController.create)
-foodsRoutes.put("/:id", foodsController.update)
-foodsRoutes.delete("/:id", foodsController.delete)
-foodsRoutes.patch("/:id", foodsPictureController.update)
+foodsRoutes.get('/:id', foodsController.show)
+foodsRoutes.get('/', foodsController.index)
+foodsRoutes.post('/', upload.single('picture'), foodsController.create)
+foodsRoutes.put('/:id', foodsController.update)
+foodsRoutes.delete('/:id', foodsController.delete)
+foodsRoutes.patch(
+	'/picture/:id',
+	upload.single('picture'),
+	foodsPictureController.update
+)
 
 module.exports = foodsRoutes
