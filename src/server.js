a32c3ea821266794a60603a42bb2232460fd7b79
lucas <lucas327ks@gmail.com>
2023-08-02 11:50:43 -0300

    changes for deployment

--- src/server.js
+++ src/server.js
@@ -1,50 +1,51 @@
-require("express-async-errors")
-require("dotenv/config")
+require('express-async-errors')
+require('dotenv/config')
 
-const express = require("express")
-const routes = require("./routes")
-const AppError = require("./utils/AppError")
-const database = require("./database/sqlite")
+const express = require('express')
+const routes = require('./routes')
+const AppError = require('./utils/AppError')
+const database = require('./database/sqlite')
 
-const uploadsConfig = require("../src/configs/uploads")
+const uploadsConfig = require('../src/configs/uploads')
 
-const cors = require("cors")
+const cors = require('cors')
 
 const app = express()
-app.use(cors({
-  origin: "http://127.0.0.1:5173"
-}))
+app.use(
+	cors({
+		origin: 'http://127.0.0.1:5173'
+	})
+)
 
-const PORT = 3333
+const PORT = 10000
 
 app.use(express.json())
 app.use(routes)
 
-app.use("/files", express.static(uploadsConfig.UPLOADS_FOLDER))
+app.use('/files', express.static(uploadsConfig.UPLOADS_FOLDER))
 
 database()
 
-app.use(( error, req, res, next) => {
-  if(error instanceof AppError){
-    return res.status(error.statusCode).json({
-      status: "error",
-      message: error.message
-    })
-  }
-  console.error(error)
+app.use((error, req, res, next) => {
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			status: 'error',
+			message: error.message
+		})
+	}
+	console.error(error)
 
-  return res.status(500).json({
-    status: "error",
-    message: "Internal server error"
-  })
+	return res.status(500).json({
+		status: 'error',
+		message: 'Internal server error'
+	})
 })
 
 app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT}`)
+	console.log(`server is running at port ${PORT}`)
 })
 
 // app.get("/:username", (req, res) => {
 //   const {username} = req.params
 //   res.send(`Hello, world! my name is ${username}`)
 // })
-
